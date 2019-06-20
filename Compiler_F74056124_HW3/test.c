bea46d4e61f943afb41d43669aa5a84812f0f0c0
JCP1014 <believe861014@gmail.com>
2019-06-20 23:06:05 +0800

    Complete all tasks

--- Compiler_F74056124_HW3/test.c
+++ Compiler_F74056124_HW3/test.c
@@ -1,28 +1,13 @@
 int a = 9;
+bool d;
 bool foo(){
 
 	bool b = true;
 	return b;
 }
 void main(){
-   if(a>3){
-	a -=3;
-	if(a<=0){
-		print("error");
-	}else if(a==6){
-		int b = 0;
-		print(b);
-		while(b < 8){
-			print(b);
-			b++;
-		}
-	}
-}
-	
-	if(a>=2){
-		bool c = foo();
-		print(c);
-	}
-	return;
+	float b = 1.0;
+	int c;
+	c = a / 0;
 }
 
